642da19d77bb743fc22e2d8b9bd5d23a84a68ffb
Dallen Sandstrom <dallensandstrom@mail.weber.edu>
2026-05-15 15:47:49 -0600

    Added user account views

--- TrellYeahCapstone/Program.cs
+++ TrellYeahCapstone/Program.cs
@@ -8,9 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)// Dallen - options.SignIn.RequireConfirmedAccount boolean changes whether users require email confirmation
+    .AddRoles<IdentityRole>() //Dallen - Used for role-based authorization.
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
@@ -31,6 +33,7 @@ else
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthorization(); // Dallen - Added line for role-based authorization.
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -42,5 +45,29 @@ app.MapControllerRoute(
 
 app.MapRazorPages()
    .WithStaticAssets();
+
+// Dallen - This block of code ensures that roles are created and seeded in the database when the application starts.
+using (var scope = app.Services.CreateScope())
+{
+    //Creates the Roles
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+
+    //if the roles do not exist add them
+    string[] roles = {"Admin"};
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+
+
+//    await SeedData.InitializeAsync(scope.ServiceProvider); // Does not copy paste cleanly since we don't have the SeedData class
+}
 
 app.Run();
