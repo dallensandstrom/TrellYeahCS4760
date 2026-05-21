2238dc5e00c088e920af6a385a7d27ca3589309e
Julie Nguyen <julienguyen2@mail.weber.edu>
2026-05-21 14:53:15 -0700

    Fix product version

--- TrellYeahCapstone/Migrations/ApplicationDbContextModelSnapshot.cs
+++ TrellYeahCapstone/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace TrellYeahCapstone.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.8")
+                .HasAnnotation("ProductVersion", "10.0.7")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
