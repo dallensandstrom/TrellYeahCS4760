438ede79e01f5f8ac60da77950b42f58fa068beb
Dallen Sandstrom <dallensandstrom@mail.weber.edu>
2026-07-05 22:08:22 -0600

    Created GrantReport model, GrantReportCreateViewModel, SubmitReport Get and Post actions to UserDashboardController, and empty SubmitReport view

---
+++ TrellYeahCapstone/Models/GrantReport.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrellYeahCapstone.Models
+{
+    public class GrantReport
+    {
+        public int GrantReportId { get; set; }
+
+        public int GrantId { get; set; }
+
+        public GrantAllocation? Grant { get; set; }
+
+        public DateTime SubmissionDate { get; set; }
+
+        [Required]
+        [StringLength(5000)]
+        public string ProjectSummary { get; set; } = string.Empty;
+
+
+        [Required]
+        [StringLength(5000)]
+        public string CurrentProgress { get; set; } = string.Empty;
+
+
+        [Required]
+        [StringLength(5000)]
+        public string NextSteps { get; set; } = string.Empty;
+
+
+        [Required]
+        [StringLength(5000)]
+        public string Budget { get; set; } = string.Empty;
+
+
+        [StringLength(500)]
+        public string? ReportFilePath { get; set; }
+
+        [NotMapped]
+        public IFormFile? ReportFile { get; set; }
+
+    }
+}
---
+++ TrellYeahCapstone/Models/GrantReportCreateViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrellYeahCapstone.Models
+{
+    public class GrantReportCreateViewModel
+    {
+        public int GrantId { get; set; }
+
+        public string ProjectDirector { get; set; } = string.Empty;
+
+        public string ProjectTitle { get; set; } = string.Empty;
+
+        public int AccountNumber { get; set; }
+
+        public DateTime SubmissionDate { get; set; }
+
+        public DateTime? AwardDate { get; set; }
+
+        [Required]
+        public string ProjectSummary { get; set; } = string.Empty;
+
+        [Required]
+        public string CurrentProgress { get; set; } = string.Empty;
+
+        [Required]
+        public string NextSteps { get; set; } = string.Empty;
+
+        [Required]
+        public string Budget { get; set; } = string.Empty;
+
+        public IFormFile? ReportFile { get; set; }
+    }
+}
